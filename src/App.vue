c9db9ae0d830e943ee2e9ccaa252c594958fb901
boosterjz <valera.borisencko@yandex.ru>
2022-01-19 12:50:53 +0300

    Add Appointment page

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,36 @@ h3 {
   padding-right: 20px;
   margin: 15px;
 }
+
+.top-button {
+  width:40px;
+  border:1px solid #8698A9;
+  background:#D0E7FD;
+  text-align:center;
+  padding:10px;
+  position:fixed;
+  bottom: 20px;
+  right:30px;
+  cursor: pointer;
+  color:#3A5B7B;
+  font-size:10px;
+  border-radius: 8px;
+  -moz-border-radius: 8px;
+  -webkit-border-radius: 8px;
+  -khtml-border-radius: 8px;
+  z-index: 1;
+}
+
+.separator{
+  border-top: 1px solid #8698A9 ;
+  border-bottom: 1px solid #8698A9 ;
+  height: 50px;
+  position: absolute;
+  left: -70px;
+  right: 0;
+  margin-top: 50px;
+  background-color: #D1E8FD;
+}
 </style>
 <script>
 import Header from "@/components/Header";
--- src/views/Appointment.vue
+++ src/views/Appointment.vue
@@ -1,13 +1,110 @@
-<template>
-<div></div>
+<template><main class="container">
+  <a href="#" title="Вернуться к началу" class="top-button">Наверх</a>
+  <div class="centered">
+    <h1>
+      Записаться на приём
+    </h1>
+    <img :src="require('@/assets/img/Doctor_logo.svg')" alt="Красивый доктор" class="doctor-logo">
+  </div>
+  <div class="form">
+    <div class="form-wrapper">
+      <form action="">
+        <label for="name">Ваши фамилия, имя, отчество</label>
+        <input type="text" id="name" v-model="name">
+        <label for="contact">Ваш E-mail или номер телефона</label>
+        <input type="text" id="contact" v-model="contact">
+        <label for="message">Ваше сообщение</label>
+        <textarea id="message" v-model="message"></textarea>
+        <button type="button" v-on:click="onClick">Отправить</button>
+      </form>
+    </div>
+  </div>
+</main>
+  <div class="separator"></div>
 </template>
 
 <script>
 export default {
-  name: "Appointment"
+  name: "Appointment",
+  data: function() {
+    return {
+      name: "",
+      contact: "",
+      message: ""
+    }
+  },
+  methods: {
+    onClick() {
+
+    }
+  }
 }
 </script>
 
 <style scoped>
 
+main {
+  display: flex;
+  flex-flow: column;
+}
+
+.centered {
+  display: flex;
+  border-bottom: 1px solid #8698A9;
+  width: 70%;
+  margin: 60px auto 0;
+  align-items: baseline;
+}
+
+.doctor-logo {
+  margin: 0 0 10px 5px;
+  height: 50px;
+}
+
+.form {
+  background-color: #D0E7FD;
+  width: 70%;
+  margin: 50px auto 0;
+  border-radius: 50px;
+}
+
+.form-wrapper {
+  margin: 0 auto;
+  width: 70%;
+  padding: 15px 0 25px;
+}
+
+input, textarea {
+  display: block;
+  margin: 12px 0;
+  width: 100%;
+  height: 30px;
+  background-color: #E5F0FA;
+  border: none;
+  border-radius: 10px;
+  padding: 10px;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+}
+
+textarea {
+  resize: none;
+  height: 200px;
+}
+
+button {
+  background-color: #E5F0FA;
+  border: none;
+  border-radius: 10px;
+  width: 180px;
+  height: 30px;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  color: #3A5B7A;
+}
+h1 {
+  font-weight: 400;
+  margin-bottom: 0;
+}
 </style>
